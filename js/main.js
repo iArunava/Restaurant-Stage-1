15c2a1bbcdb82add441855a9970335c0a179cf8f
Arunava <learningdroidarunava@gmail.com>
2018-06-27 13:55:21 +0530

    refactor: Changed the code to return the location of the exact image
    Using window.location to fetch the exact location of the image files

--- js/main.js
+++ js/main.js
@@ -149,7 +149,7 @@ createRestaurantHTML = (restaurant) => {
     const image = document.createElement('img');
     image.className = 'restaurant-img';
     image.alt = restaurant.name;
-    image.src = DBHelper.imageUrlForRestaurant(restaurant);
+    image.src = window.location.href + DBHelper.imageUrlForRestaurant(restaurant);
     li.append(image);
 
     const name = document.createElement('h1');
